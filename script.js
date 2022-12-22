bd23f74a7fbaa3113f0fcd67f97f6b78f42812f0
nhopo <thanh.le.nguyen@rwth-aachen.de>
2022-12-22 10:33:24 +0100

    Change operand declaration to ""

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function multiply(a,b){
     return a * b;
 }
 function divide(a,b){
+    if (b==0) {
+        return "yikes..";
+    }
     return a/b;
 }
 
@@ -28,68 +31,85 @@ function operate(operator, a, b){
     }
 }
 
+function clearAll(){
+    firstNum = "";
+    secondNum = "";
+    firstNumSet = false;
+    display.textContent = 0;
+}
+
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const display = document.querySelector('.output-display');
 const equal = document.querySelector('#equal-sign');
+const clear = document.querySelector('#clear-btn');
 
-function calculate(){
-    let firstNum = 0;
-    let secondNum = 0;
-    let result;
-    let operator;
-    let firstNumSet = false;
-    let secondNumSet = false;
+let firstNum = "";
+let secondNum = "";
+let result;
+let operator;
+let firstNumSet = false;
+let calculationDone = false;
 
-    digits.forEach(button => {
-        button.addEventListener('click', () => {
+clear.addEventListener('click', clearAll());
 
-            if (!firstNumSet){
-                firstNum += button.textContent;
-                display.textContent = Number(firstNum);
-                console.log(`First Num: ${Number(firstNum)}`);
-            } else if (!secondNumSet){
-                secondNum += button.textContent;
-                display.textContent = Number(secondNum);
-                console.log(`Second Num: ${Number(secondNum)}`);
+digits.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!firstNumSet || operator == ""){
+            if(calculationDone) {
+                firstNum = "";
             }
-        });
+            firstNum += button.textContent;
+            display.textContent = Number(firstNum);
+            console.log(`Digits First Num: ${Number(firstNum)}`);
+        } else {
+            secondNum += button.textContent;
+            display.textContent = Number(secondNum);
+            console.log(`Digits Second Num: ${Number(secondNum)}`);
+        }
     });
+});
 
-    operators.forEach(button => {
-        button.addEventListener('click', () => {
-            
-            if(firstNumSet)
-            {
-                result = operate(operator, Number(firstNum), Number(secondNum));
-                display.textContent = result;
-                firstNum = result;
-                secondNum = 0;
-                console.log(`First Num: ${Number(firstNum)}`);
-                console.log(`Second Num: ${Number(secondNum)}`);
-            }
-            operator = button.id;
-            firstNumSet = true;
-
-            console.log(operator);
-        });
-    });
-
-    equal.addEventListener('click', () => {
-        if(firstNumSet && typeof operator != 'undefined'){
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        
+        // 12 + 7 +
+        if(firstNumSet && secondNum != "")
+        {
             result = operate(operator, Number(firstNum), Number(secondNum));
-            firstNum = result;
-            secondNum = 0;
             display.textContent = result;
-            console.log(`RESULT: ${result} FIRSTNUM: ${firstNum} OPERATOR: ${operator}` );
+            if (typeof result != Number) calculationDone = true;    
+            firstNum = result;
+            secondNum = "";
+            console.log(`Operators First Num: ${firstNum}`);
+            console.log(`Operators Second Num: ${secondNum}`);
         }
-        else {
-            firstNum = 0;
-            secondNum = 0;
-            console.log("ERROR");
-        }
+
+        // 12 +
+        // +
+        // 12 ++
+        operator = button.id;
+        firstNumSet = true;
+
+        console.log(operator);
     });
-}
+});
 
-
-calculate();
+equal.addEventListener('click', () => {
+    // (12 + 7) + 5 =
+    if(firstNumSet && secondNum != "" && operator != ""){
+        result = operate(operator, Number(firstNum), Number(secondNum));
+        firstNum = result;
+        secondNum = "";
+        operator = "";
+        display.textContent = result;
+        calculationDone = true;
+        console.log(`RESULT: ${result} FIRSTNUM: ${firstNum} OPERATOR: ${operator}` );
+    }
+    // 12 + =
+    else {
+        console.log("Do nothing.")
+        operator = "";
+        console.log(operator);
+    }
+});
